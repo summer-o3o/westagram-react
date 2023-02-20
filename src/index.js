ff1e0202a3d55e6a51df3e6f2650404efc5ab715
summer-o3o <ho_ho95@naver.com>
2023-02-20 20:08:04 +0900

    ADD : [Assginment 1-2] Westagram 세팅하기

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+import "./pages/Style/common.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render( <Login />);
+root.render( <Main />);
