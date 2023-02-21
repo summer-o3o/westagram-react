e8d303237cb8ce79e779f1ff7c277f20231f1d87
summer-o3o <ho_ho95@naver.com>
2023-02-21 17:58:33 +0900

    ADD : Router & Sass : Westagram Router와 Sass 적용하기

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
-import "./pages/Style/common.css";
+import Router from './Router';
+import "./pages/Style/reset.scss";
+import "./pages/Style/common.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render( <Login />);
-root.render( <Main />);
+root.render( <Router />);
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,13 @@
 import React from "react";
-import "./Login.css";
+import "./Login.scss";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate("/main");
+  };
   return (
     <div className="wrap_login">
       <h2 className="screen_out">인스타그램</h2>
@@ -12,7 +18,7 @@ const Login = () => {
               <legend className="screen_out">로그인</legend>
               <input type="text" className="inp_user" placeholder="전화번호, 사용자이름 또는 이메일" title="전화번호, 사용자이름 또는 이메일" />
               <input type="password" className="inp_psassword" placeholder="비밀번호" title="비밀번호"  />
-              <button type="submit" className="btn_login" disabled>로그인</button>{/* 아이디, 비밀번호 입력시 속성 disabled 제거 */}
+              <button type="submit" className="btn_login"  onClick={goToMain}>로그인</button>{/* 아이디, 비밀번호 입력시 속성 disabled 제거 */}
             </fieldset>
           </form>
           <a href="#none" className="link_psassword">비밀번호를 잊으셨나요?</a>
